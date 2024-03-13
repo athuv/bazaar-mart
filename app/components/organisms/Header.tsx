4912eb1a45dba0571b99c68cc69c484f75cddd95
Ahamed <21102277+athuv@users.noreply.github.com>
2024-03-13 21:19:41 +0530

    feat: Implement ProfileAndCartLink component in Header component

--- app/components/organisms/Header.tsx
+++ app/components/organisms/Header.tsx
@@ -1,10 +1,14 @@
 import MenuButtonAndLogo from "@/app/components/molecules/MenuButtonAndLogo";
+import ProfileAndCartLink from "@/app/components/molecules/ProfileAndCartLink";
 import React from "react";
 
 function Header() {
   return (
     <header className="bg-primary px-2 py-2">
-      <MenuButtonAndLogo />
+      <div className="flex justify-between">
+        <MenuButtonAndLogo />
+        <ProfileAndCartLink />
+      </div>
     </header>
   );
 }
