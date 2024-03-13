9e96111b8659b53aca39cce63476438943935c16
Ahamed <21102277+athuv@users.noreply.github.com>
2024-03-13 20:44:50 +0530

    feat: Add HomeTemplate template component and stories

---
+++ app/components/templates/Home.stories.tsx
@@ -0,0 +1,24 @@
+import type { Meta, StoryFn, StoryObj } from "@storybook/react";
+
+import HomeTemplate from "./HomeTemplate";
+
+// const withTailwindClasses = (Story: StoryFn) => (
+//   <div className="w-screen bg-primary p-2">
+//     <Story />
+//   </div>
+// );
+
+const meta = {
+  title: "templates/Home",
+  component: HomeTemplate,
+  // decorators: [withTailwindClasses],
+  parameters: {
+    layout: "fullscreen",
+  },
+  tags: ["autodocs"],
+} satisfies Meta<typeof HomeTemplate>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Home: Story = {};
---
+++ app/components/templates/HomeTemplate.tsx
@@ -0,0 +1,14 @@
+import Header from "@/app/components/organisms/Header";
+import Home from "@/app/page";
+import React from "react";
+
+function HomeTemplate() {
+  return (
+    <>
+      <Header />
+      <Home />
+    </>
+  );
+}
+
+export default HomeTemplate;
