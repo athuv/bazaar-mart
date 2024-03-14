bcf7434bed0bc7cdafc000f68a1092c3b0fc519e
Ahamed <21102277+athuv@users.noreply.github.com>
2024-03-14 09:35:42 +0530

    feat: Implement DeliveredToSection component in Home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import DeliveredToSection from "@/app/components/organisms/DeliveredToSection";
 import SearchSection from "@/app/components/organisms/SearchSection";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between">
+    <main className="flex min-h-screen flex-col items-center ">
       <SearchSection />
+      <DeliveredToSection />
     </main>
   );
 }
