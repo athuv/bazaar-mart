eb6c1e66abb77cbd4d894554e5e939193fdd08d6
Ahamed <21102277+athuv@users.noreply.github.com>
2024-03-18 22:58:22 +0530

    feat: Add CategoryHorizSection molecule component & stories

---
+++ app/components/organisms/CategoryHorizSection.tsx
@@ -0,0 +1,13 @@
+import CategoryHorizList from "@/app/components/molecules/CategoryHorizList";
+import React from "react";
+import categories from "@/lib/data/categories.json";
+
+function CategoryHorizSection() {
+  return (
+    <section className="flex w-screen bg-primary">
+      <CategoryHorizList categoryList={categories} />
+    </section>
+  );
+}
+
+export default CategoryHorizSection;
