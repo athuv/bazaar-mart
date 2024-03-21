550096ff7aeba75ca9e7ba2cf754ec8c52e35f3b
Ahamed <21102277+athuv@users.noreply.github.com>
2024-03-21 16:50:22 +0530

    feat: Implement LoginSignupSection commponent in Home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import CategoryHorizSection from "@/app/components/organisms/CategoryHorizSection";
 import DeliverySelectionSection from "@/app/components/organisms/DeliverySelectionSection";
+import LoginSignupSection from "@/app/components/organisms/LoginSignupSection";
 import SearchSection from "@/app/components/organisms/SearchSection";
 
 export default function Home() {
@@ -8,6 +9,7 @@ export default function Home() {
       <SearchSection />
       <CategoryHorizSection />
       <DeliverySelectionSection />
+      <LoginSignupSection />
     </main>
   );
 }
