c7bf2ea66d80dde75ee1f5d5dac596c7be88a61e
Ahamed <21102277+athuv@users.noreply.github.com>
2024-03-21 15:23:54 +0530

    feat: Implement MenuButtonAndLogo component in Home page

--- app/components/molecules/MenuButtonAndLogo.tsx
+++ app/components/molecules/MenuButtonAndLogo.tsx
@@ -1,11 +1,12 @@
-import HamburgerMenuButton from "@/app/components/atoms/buttons/HamburgerMenuButton";
 import MobileLogo from "@/app/components/atoms/logo/MobileLogo";
+import SlideDrawerWithToggle from "@/app/components/molecules/SlideDrawerWithToggle";
+import categories from "@/lib/data/categories.json";
 import React from "react";
 
 function MenuButtonAndLogo() {
   return (
     <div className="flex items-center gap-1">
-      <HamburgerMenuButton />
+      <SlideDrawerWithToggle categoryList={categories} />
       <MobileLogo />
     </div>
   );
