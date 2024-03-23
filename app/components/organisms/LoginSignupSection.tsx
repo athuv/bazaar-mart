c1f0e20f09f67cce0f91b0676083d8c9c1c0da2d
Ahamed <21102277+athuv@users.noreply.github.com>
2024-03-23 10:37:04 +0530

    feat:Add Seperator shadcn component for LoginSignupSection component

--- app/components/organisms/LoginSignupSection.tsx
+++ app/components/organisms/LoginSignupSection.tsx
@@ -1,10 +1,13 @@
+import { Separator } from "@/app/components/atoms/shadcn/separator";
 import LoginSignup from "@/app/components/molecules/LoginSignup";
 import React from "react";
 
 function LoginSignupSection() {
   return (
     <section className="w-screen">
+      <Separator />
       <LoginSignup />
+      <Separator />
     </section>
   );
 }
