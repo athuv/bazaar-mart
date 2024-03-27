1082a049975275526c992bd8c255d8d9e434b5f8
Ahamed <21102277+athuv@users.noreply.github.com>
2024-03-27 14:04:24 +0530

    feat: Implement categories mock data in CategoryHorizList story

--- app/components/molecules/CategoryHorizList.stories.tsx
+++ app/components/molecules/CategoryHorizList.stories.tsx
@@ -1,6 +1,7 @@
 import type { Meta, StoryFn, StoryObj } from "@storybook/react";
 
 import CategoryHorizList from "./CategoryHorizList";
+import categoryList from "@/lib/data/categories.json";
 
 const withTailwindClasses = (Story: StoryFn) => (
   <div className="w-screen bg-primary">
@@ -23,13 +24,6 @@ type Story = StoryObj<typeof meta>;
 
 export const CategoryHorizontalList: Story = {
   args: {
-    categoryList: [
-      { id: "001", category: "Electronics", link: "./" },
-      { id: "002", category: "Food", link: "./" },
-      { id: "003", category: "Book", link: "./" },
-      { id: "004", category: "Clothing", link: "./" },
-      { id: "005", category: "Personal Care", link: "./" },
-      { id: "006", category: "Furniture", link: "./" },
-    ],
+    categoryList,
   },
 };
