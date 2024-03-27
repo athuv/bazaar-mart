2c2ebdf6e0673434b6c02a894dcbc47c8bf668da
Ahamed <21102277+athuv@users.noreply.github.com>
2024-03-27 17:41:34 +0530

    style: Change Footer component width properties

--- app/components/organisms/Footer.tsx
+++ app/components/organisms/Footer.tsx
@@ -7,12 +7,12 @@ import React from "react";
 
 function Footer() {
   return (
-    <footer className="bg-primary px-2 py-2">
-      <section>
+    <footer className="w-screen bg-primary px-2 py-2">
+      <section className="w-full">
         <FooterUpper />
       </section>
       <Separator orientation="horizontal" className="my-4" />
-      <section className="w-screen">
+      <section className="w-full">
         <FooterLower />
       </section>
     </footer>
