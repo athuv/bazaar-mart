dd0f8ec7311390cb21c9a63a4c0f9be18ba91bed
Ahamed <21102277+athuv@users.noreply.github.com>
2024-04-04 19:45:51 +0530

    feat:Add 'All' button for desktop view

--- app/components/molecules/CategoryHorizList.tsx
+++ app/components/molecules/CategoryHorizList.tsx
@@ -6,11 +6,23 @@ import {
   ScrollBar,
 } from "@/app/components/atoms/shadcn/scroll-area";
 import { Category } from "@/lib/types/categoryTypes";
+import { Button } from "@/app/components/atoms/shadcn/button";
+import { Menu } from "lucide-react";
 
 function CategoryHorizList({ categoryList }: Category) {
   return (
     <ScrollArea>
-      <div className="flex h-fit flex-grow gap-3 px-2 py-2 pb-[0.85rem]">
+      <div className="flex h-fit flex-grow items-center gap-3 px-2 py-2 pb-[0.85rem] md:px-32 md:py-1">
+        <div className="hidden md:block">
+          <Button
+            className="flex h-fit w-fit items-center gap-1 px-1"
+            variant="ghost"
+            size="icon"
+          >
+            <Menu size={18} />
+            <span>All</span>
+          </Button>
+        </div>
         {categoryList.map((_category) => (
           <CategoryHorizLink link={_category.link} key={_category.id}>
             {_category.category}
