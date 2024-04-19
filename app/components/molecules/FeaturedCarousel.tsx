97f2b446de762aff0fbe249cebf96de1ca6bdf4a
Ahamed <21102277+athuv@users.noreply.github.com>
2024-04-19 14:16:55 +0530

    feat: Change styles & implement responsive images

--- app/components/molecules/FeaturedCarousel.tsx
+++ app/components/molecules/FeaturedCarousel.tsx
@@ -1,41 +1,50 @@
-import React from "react";
+import Image from "next/image";
+
 import {
   Carousel,
   CarouselContent,
   CarouselItem,
 } from "@/app/components/atoms/shadcn/carousel";
-import { Card, CardContent } from "@/app/components/atoms/shadcn/card";
+
 import { FeaturedImage } from "@/lib/types/types";
-import Image from "next/image";
-import FeaturedScroll from "@/app/components/molecules/FeaturedScroll";
+import CategoryListCard from "@/app/components/molecules/desktop/CategoryListCard";
 
-function FeaturedCarousel({ slider, scroll }: FeaturedImage) {
+function FeaturedCarousel({ slider }: FeaturedImage) {
   return (
-    <Carousel
-      opts={{ align: "start", loop: true }}
-      className="mb-2 h-fit w-full lg:px-16"
-    >
-      <CarouselContent>
-        {slider.map((_image) => (
-          <CarouselItem key={_image.id}>
-            <picture>
-              <source srcSet={_image.srcMobile} media="(max-width: 768px)" />
-              <source srcSet={_image.srcDesktop} media="(min-width: 768px)" />
-              <Image
-                alt="image"
-                src="/images/featured/1.jpg"
-                width={1236}
-                height={1080}
-                style={{ width: "100%" }}
-              />
-            </picture>
-          </CarouselItem>
-        ))}
-      </CarouselContent>
+    <div className="gap-2 lg:flex lg:h-fit lg:justify-center lg:px-2 lg:py-4">
+      <CategoryListCard />
 
-      {/* <div className="absolute top-64 h-40 w-full bg-gradient-to-b from-card via-background  blur-sm"></div>
-       <FeaturedScroll scroll={scroll} /> */}
-    </Carousel>
+      <div className="flex items-center lg:rounded-md lg:bg-secondary">
+        <Carousel
+          opts={{ align: "start", loop: true }}
+          className="lg:fit mb-2 h-fit w-full lg:mb-0"
+        >
+          <CarouselContent>
+            {slider.map((_image) => (
+              <CarouselItem className="lg:flex-grow" key={_image.id}>
+                <picture>
+                  <source
+                    srcSet={_image.srcMobile}
+                    media="(max-width: 768px)"
+                  />
+                  <source
+                    srcSet={_image.srcDesktop}
+                    media="(min-width: 768px)"
+                  />
+                  <Image
+                    alt="image"
+                    src="/images/featured/1.jpg"
+                    width={1236}
+                    height={1080}
+                    className="h-auto w-auto lg:rounded-sm"
+                  />
+                </picture>
+              </CarouselItem>
+            ))}
+          </CarouselContent>
+        </Carousel>
+      </div>
+    </div>
   );
 }
 
