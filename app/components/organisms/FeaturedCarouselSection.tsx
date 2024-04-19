18f94c2f7d8aaf158bc5d662be4cf1be6cf862ec
Ahamed <21102277+athuv@users.noreply.github.com>
2024-04-19 13:59:40 +0530

    feat:Add & implement API for carousel contents

--- app/components/organisms/FeaturedCarouselSection.tsx
+++ app/components/organisms/FeaturedCarouselSection.tsx
@@ -1,14 +1,27 @@
-import React from "react";
-import featuredImages from "@/lib/data/featuredImages.json";
 import FeaturedCarousel from "@/app/components/molecules/FeaturedCarousel";
 
-function FeaturedCarouselSection() {
+async function getCarouselImage() {
+  const response = await fetch(
+    `${process.env.BASE_URL}api/v1/carouselContent`,
+    {
+      cache: "force-cache",
+    },
+  );
+
+  if (!response.ok) {
+    throw new Error("Failed to fetch data");
+  }
+
+  const data = await response.json();
+  return data;
+}
+
+async function FeaturedCarouselSection() {
+  const carouselImages = await getCarouselImage();
+
   return (
     <section className="w-full">
-      <FeaturedCarousel
-        slider={featuredImages.slider}
-        scroll={featuredImages.scroll}
-      />
+      <FeaturedCarousel slider={carouselImages.slider} />
     </section>
   );
 }
