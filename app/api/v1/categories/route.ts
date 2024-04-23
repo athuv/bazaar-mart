e93b32eed5dd1578d07a9e0323123f2685f6cdba
Ahamed <21102277+athuv@users.noreply.github.com>
2024-04-23 17:30:19 +0530

    feat:Implement categories API route & GET method to get main categories

---
+++ app/api/v1/categories/route.ts
@@ -0,0 +1,16 @@
+import { getMainCategories } from "@/lib/db/drizzle/queries";
+import { NextRequest } from "next/server";
+
+export async function GET(request: NextRequest) {
+  const type = request.nextUrl.searchParams.get("type");
+
+  if (type === "main") {
+    const limitParam = request.nextUrl.searchParams.get("limit");
+    const limit = Number(limitParam);
+    const validLimit = !isNaN(limit) ? limit : 0;
+
+    const mainCategories = await getMainCategories(validLimit);
+
+    return Response.json({ mainCategories });
+  }
+}
