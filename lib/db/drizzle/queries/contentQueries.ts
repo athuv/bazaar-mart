18988f02c1bf6b3341a9fb4eeac39814807e26b6
Ahamed <21102277+athuv@users.noreply.github.com>
2024-04-23 22:24:11 +0530

    refactor: Rename getSliderImages() function to getSliderImagesQuery()

--- lib/db/drizzle/queries/contentQueries.ts
+++ lib/db/drizzle/queries/contentQueries.ts
@@ -4,7 +4,7 @@ import { db } from "@/lib/db/drizzle";
 import { sliderImagesTable } from "@/lib/db/drizzle/schemas";
 import { eq } from "drizzle-orm";
 
-export async function getSliderImages() {
+export async function getSliderImagesQuery() {
   const sliderImages = await db.query.sliderImagesTable.findMany({
     where: eq(sliderImagesTable.isActive, true),
   });
