c2f6cf39da179f2770f55040e3b20ccf03ebb30a
Ahamed <21102277+athuv@users.noreply.github.com>
2024-04-23 22:52:06 +0530

    feat:Remove BASE_URL from categoriesApi

--- lib/api/categoriesApi.ts
+++ lib/api/categoriesApi.ts
@@ -3,12 +3,9 @@ import { Category, GetMainCategories } from "@/lib/types/types";
 export async function getMainCategories({
   limit,
 }: GetMainCategories): Promise<Category[]> {
-  const response = await fetch(
-    `${process.env.BASE_URL}api/v1/categories?type=main&limit=${limit}`,
-    {
-      cache: "force-cache",
-    },
-  );
+  const response = await fetch(`api/v1/categories?type=main&limit=${limit}`, {
+    cache: "force-cache",
+  });
 
   if (!response.ok) {
     throw new Error("Failed to fetch data");
