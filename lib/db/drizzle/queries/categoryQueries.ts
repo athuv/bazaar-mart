ae48f41ed25372e62d0efb85c5f640d9a27a84fb
Ahamed <21102277+athuv@users.noreply.github.com>
2024-04-23 22:25:20 +0530

    feat:Implement type annotations in getMainCategoriesQuery() function

--- lib/db/drizzle/queries/categoryQueries.ts
+++ lib/db/drizzle/queries/categoryQueries.ts
@@ -3,8 +3,11 @@ import "server-only";
 import { db } from "@/lib/db/drizzle";
 import { categoriesTable } from "@/lib/db/drizzle/schemas";
 import { isNull } from "drizzle-orm";
+import { Category, GetMainCategories } from "@/lib/types/types";
 
-export async function getMainCategories(limit = 0) {
+export async function getMainCategoriesQuery({
+  limit,
+}: GetMainCategories): Promise<Category[]> {
   const mainCategories = await db.query.categoriesTable.findMany({
     where: isNull(categoriesTable.parentId),
     limit: limit,
