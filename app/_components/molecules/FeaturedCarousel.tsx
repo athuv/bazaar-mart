d831c4dd84a13d2ad3acdc633c21e1ab6bf460d0
Ahamed <21102277+athuv@users.noreply.github.com>
2024-04-26 23:00:42 +0530

    feat:Remove HTML 'picture' element & immplement 'next/image' component

--- app/_components/molecules/FeaturedCarousel.tsx
+++ app/_components/molecules/FeaturedCarousel.tsx
@@ -10,42 +10,51 @@ import { ResponsiveImage } from "@/lib/types/types";
 import { Skeleton } from "@/app/_components/atoms/shadcn/skeleton";
 
 async function FeaturedCarousel() {
-  const carouselImages: ResponsiveImage[] = await getSliderImagesQuery();
-  // const carouselImages: ResponsiveImage[] = [];
+  // const carouselImages: ResponsiveImage[] = await getSliderImagesQuery();
+  const carouselImages: ResponsiveImage[] = [];
   return (
-    <div className="flex min-h-[250px] items-center lg:max-h-[402px] lg:w-full lg:rounded-md lg:bg-secondary">
+    <div className="flex items-center lg:max-h-[402px] lg:w-full lg:rounded-md lg:bg-secondary">
       <Carousel
         opts={{ align: "start", loop: true }}
         className="lg:fit mb-2 h-fit w-full lg:mb-0"
       >
         <CarouselContent>
           {carouselImages.length <= 0 && (
-            <CarouselItem className="min-h-[246px] lg:flex-grow">
-              <Skeleton className="max-h-[384px] min-h-[246px] lg:h-[402px] lg:min-w-[778px] lg:max-w-[1023px]" />
-            </CarouselItem>
-          )}
-          {carouselImages.map((_image) => (
-            <CarouselItem
-              className="min-h-[246px] lg:flex-grow"
-              key={_image.sliderImageId}
-            >
-              <picture>
-                <source
-                  srcSet={`/images/featured/${_image.imageMobile}`}
-                  media="(max-width: 768px)"
-                />
-                <source
-                  srcSet={`/images/featured/${_image.imageDesktop}`}
-                  media="(min-width: 768px)"
+            <CarouselItem className="lg:flex-grow">
+              <Skeleton className="">
+                <Image
+                  alt="image"
+                  src="/images/slider-images/default-min-768px.webp"
+                  width={768}
+                  height={504}
+                  className="object-contain md:hidden"
                 />
                 <Image
                   alt="image"
-                  src="/images/featured/1.jpg"
-                  width={1236}
-                  height={1080}
-                  className="h-auto  w-auto lg:rounded-sm"
+                  src="/images/slider-images/default-max-768px.webp"
+                  width={1023}
+                  height={375}
+                  className="hidden object-contain md:inline lg:rounded-sm"
                 />
-              </picture>
+              </Skeleton>
+            </CarouselItem>
+          )}
+          {carouselImages.map((_image) => (
+            <CarouselItem className="lg:flex-grow" key={_image.sliderImageId}>
+              <Image
+                alt={_image.imageAlt!}
+                src={`/images/slider-images/${_image.imageMobile}`}
+                width={768}
+                height={503}
+                className="object-contain md:hidden"
+              />
+              <Image
+                alt={_image.imageAlt!}
+                src={`/images/slider-images/${_image.imageDesktop}`}
+                width={1023}
+                height={375}
+                className="hidden object-contain md:inline lg:rounded-sm"
+              />
             </CarouselItem>
           ))}
         </CarouselContent>
