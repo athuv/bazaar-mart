19b5d5144582636259c6d0259f49eb6868f7a9ea
Ahamed <21102277+athuv@users.noreply.github.com>
2024-04-26 22:26:57 +0530

    style:Remove max-height & min-height TailwindCSS classes

--- app/_components/organisms/CarouselAndCategorylist.tsx
+++ app/_components/organisms/CarouselAndCategorylist.tsx
@@ -4,7 +4,7 @@ import CategoryListCard from "@/app/_components/molecules/desktop/CategoryListCa
 async function CarouselAndCategorylist() {
   return (
     <section className="w-full">
-      <div className="max-h-[384px] min-h-[217px] gap-2 lg:flex lg:max-h-[435px] lg:justify-center lg:px-2 lg:py-4">
+      <div className="gap-2 lg:flex lg:max-h-[435px] lg:justify-center lg:px-2 lg:py-4">
         <CategoryListCard />
         <FeaturedCarousel />
       </div>
