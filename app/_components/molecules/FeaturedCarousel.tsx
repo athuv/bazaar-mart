8f1250834708b07f884484d9dd95f9e2adc6639c
Ahamed <21102277+athuv@users.noreply.github.com>
2024-04-26 20:52:20 +0530

    style: fix layout shift caused by FeaturedCarousel component

--- app/_components/molecules/FeaturedCarousel.tsx
+++ app/_components/molecules/FeaturedCarousel.tsx
@@ -11,21 +11,24 @@ import { Skeleton } from "@/app/_components/atoms/shadcn/skeleton";
 
 async function FeaturedCarousel() {
   const carouselImages: ResponsiveImage[] = await getSliderImagesQuery();
-
+  // const carouselImages: ResponsiveImage[] = [];
   return (
-    <div className="flex items-center lg:max-h-[402px] lg:w-full lg:rounded-md lg:bg-secondary">
+    <div className="flex min-h-[250px] items-center lg:max-h-[402px] lg:w-full lg:rounded-md lg:bg-secondary">
       <Carousel
         opts={{ align: "start", loop: true }}
         className="lg:fit mb-2 h-fit w-full lg:mb-0"
       >
         <CarouselContent>
           {carouselImages.length <= 0 && (
-            <CarouselItem className="lg:flex-grow">
-              <Skeleton className="max-h-[384px] min-h-[217px] lg:h-[402px] lg:min-w-[778px] lg:max-w-[1023px]" />
+            <CarouselItem className="min-h-[246px] lg:flex-grow">
+              <Skeleton className="max-h-[384px] min-h-[246px] lg:h-[402px] lg:min-w-[778px] lg:max-w-[1023px]" />
             </CarouselItem>
           )}
           {carouselImages.map((_image) => (
-            <CarouselItem className="lg:flex-grow" key={_image.sliderImageId}>
+            <CarouselItem
+              className="min-h-[246px] lg:flex-grow"
+              key={_image.sliderImageId}
+            >
               <picture>
                 <source
                   srcSet={`/images/featured/${_image.imageMobile}`}
@@ -40,7 +43,7 @@ async function FeaturedCarousel() {
                   src="/images/featured/1.jpg"
                   width={1236}
                   height={1080}
-                  className="h-auto w-auto lg:rounded-sm"
+                  className="h-auto  w-auto lg:rounded-sm"
                 />
               </picture>
             </CarouselItem>
