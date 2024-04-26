c2a8434f37aa12539660010ddcce72de2fc18842
Ahamed <21102277+athuv@users.noreply.github.com>
2024-04-26 22:25:30 +0530

    style: Change main element min-height to min-h-screen tailwindCSS class

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ import SearchSection from "@/app/_components/organisms/mobile/SearchSection";
 
 export default function Home() {
   return (
-    <main className="flex min-h-svh w-full flex-col lg:mx-auto lg:max-w-6xl xl:max-w-7xl">
+    <main className="flex min-h-screen w-full flex-grow flex-col lg:mx-auto lg:max-w-6xl xl:max-w-7xl">
       <SearchSection />
       <CarouselAndCategorylist />
       <LoginSignupSection />
