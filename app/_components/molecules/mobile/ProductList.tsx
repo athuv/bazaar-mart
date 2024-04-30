5fdb0f0d6a9423df110fba873a415f200e12ecc0
Ahamed <21102277+athuv@users.noreply.github.com>
2024-04-30 16:20:09 +0530

    feat: Add dynamic data to ProductList & ProductListSection components

---
+++ app/_components/molecules/mobile/ProductList.tsx
@@ -0,0 +1,87 @@
+import Image from "next/image";
+import Link from "next/link";
+
+import { Button } from "@/app/_components/atoms/shadcn/button";
+import {
+  Card,
+  CardContent,
+  CardHeader,
+} from "@/app/_components/atoms/shadcn/card";
+
+import { Product } from "@/lib/types/types";
+import { getProductsQuery } from "@/lib/db/drizzle/queryActions";
+
+import { Rating } from "@smastrom/react-rating";
+
+async function ProductList() {
+  const products = await getProductsQuery();
+
+  return (
+    <div className="grid grid-cols-2 justify-items-center gap-y-4 py-4">
+      {products.map((_product) => {
+        console.log(_product);
+        return (
+          <Button
+            className="h-fit whitespace-normal px-0 py-0 "
+            key={_product.productId}
+            variant="ghost"
+            asChild
+          >
+            <Link href="./">
+              <Card className="w-[148px] rounded-md border-none">
+                <Image
+                  src={`/images/products/${_product.productId}/${_product.primaryImageUrl}`}
+                  alt={_product.primaryImageAlt!}
+                  width={300}
+                  height={300}
+                  className="rounded-t-md"
+                />
+
+                <CardHeader className="space-y-0 p-0 px-2 py-1">
+                  <h4 className="heading-4 line-clamp-2 font-bold">
+                    {_product.title}
+                  </h4>
+                </CardHeader>
+                <CardContent className="flex h-20 flex-col px-2 py-1">
+                  <div className="h-7 truncate">
+                    <span className="font-bold text-destructive">Rs</span>
+                    <span className="text-lg font-bold text-destructive">
+                      {`.${_product.finalPrice}`}
+                    </span>
+                  </div>
+                  <div className="flex h-5 items-center gap-1 truncate">
+                    <span className="truncate line-through">
+                      Rs.{_product.priceInAmount}
+                    </span>
+
+                    <span className="truncate text-xs">
+                      {`-${_product.discountPercentage}%`}
+                    </span>
+                  </div>
+                  <div className="flex h-6 items-center justify-start">
+                    <div className="flex h-4">
+                      <Rating
+                        style={{ maxWidth: 65 }}
+                        value={_product.averageReview}
+                        readOnly
+                      />
+                    </div>
+
+                    <div className="flex h-4 items-center text-muted-foreground">
+                      <span className="text-xs">{_product.averageReview}</span>
+                      <span className="text-[10px] text-muted-foreground">
+                        {`(${_product.totalReviews})`}
+                      </span>
+                    </div>
+                  </div>
+                </CardContent>
+              </Card>
+            </Link>
+          </Button>
+        );
+      })}
+    </div>
+  );
+}
+
+export default ProductList;
---
+++ app/_components/organisms/mobile/ProductListSection.tsx
@@ -0,0 +1,12 @@
+import ProductList from "@/app/_components/molecules/mobile/ProductList";
+import React from "react";
+
+function ProductListSection() {
+  return (
+    <section className="w-full">
+      <ProductList />
+    </section>
+  );
+}
+
+export default ProductListSection;
