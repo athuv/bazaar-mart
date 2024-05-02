3d2eec1b2a044d3a93dd0d4df7cc63f23ac60bb7
Ahamed <21102277+athuv@users.noreply.github.com>
2024-05-02 17:28:42 +0530

    feat:Add heading for ProductList component

--- app/_components/molecules/mobile/ProductList.tsx
+++ app/_components/molecules/mobile/ProductList.tsx
@@ -17,71 +17,78 @@ async function ProductList() {
   const products: BasicProductList[] = await getBasicProductsQuery();
 
   return (
-    <div className="grid auto-rows-[276px] grid-cols-[repeat(auto-fill,minmax(148px,1fr))] justify-items-center gap-2 gap-y-3 py-4">
-      {[...products, ...products, ...products, ...products].map((_product) => {
-        return (
-          <Button
-            className="h-[276px] w-[148px] whitespace-normal px-0 py-0 "
-            key={_product.productId}
-            variant="ghost"
-            asChild
-          >
-            <Link href="./">
-              <Card className="w-[148px] rounded-md border-none">
-                <Image
-                  src={`/images/products/${_product.productId}/${_product.primaryImageUrl}`}
-                  alt={_product.primaryImageAlt!}
-                  width={300}
-                  height={300}
-                  className="rounded-t-md"
-                />
+    <div>
+      <h2 className="pl-4 pt-2 text-2xl font-semibold">Available Products</h2>
+      <div className="grid auto-rows-[276px] grid-cols-[repeat(auto-fill,minmax(148px,1fr))] justify-items-center gap-2 gap-y-3 py-4">
+        {[...products, ...products, ...products, ...products].map(
+          (_product) => {
+            return (
+              <Button
+                className="h-[276px] w-[148px] whitespace-normal px-0 py-0 "
+                key={_product.productId}
+                variant="ghost"
+                asChild
+              >
+                <Link href="./">
+                  <Card className="w-[148px] rounded-md border-none">
+                    <Image
+                      src={`/images/products/${_product.productId}/${_product.primaryImageUrl}`}
+                      alt={_product.primaryImageAlt!}
+                      width={300}
+                      height={300}
+                      className="rounded-t-md"
+                    />
 
-                <CardHeader className="space-y-0 p-0 px-2 py-1">
-                  <h4 className="heading-4 line-clamp-2 font-bold">
-                    {_product.title}
-                  </h4>
-                </CardHeader>
-                <CardContent className="flex h-20 flex-col px-2 py-1">
-                  <div className="h-7 truncate">
-                    <span className="font-bold text-destructive">Rs</span>
-                    <span className="text-lg font-bold text-destructive">
-                      {`.${_product.finalPrice}`}
-                    </span>
-                  </div>
-                  {_product.discountAmount && (
-                    <div className="flex h-5 items-center gap-1 truncate">
-                      <span className="truncate line-through">
-                        Rs.{_product.priceInAmount}
-                      </span>
+                    <CardHeader className="space-y-0 p-0 px-2 py-1">
+                      <h4 className="heading-4 line-clamp-2 font-bold">
+                        {_product.title}
+                      </h4>
+                    </CardHeader>
+                    <CardContent className="flex h-20 flex-col px-2 py-1">
+                      <div className="h-7 truncate">
+                        <span className="font-bold text-destructive">Rs</span>
+                        <span className="text-lg font-bold text-destructive">
+                          {`.${_product.finalPrice}`}
+                        </span>
+                      </div>
+                      {_product.discountAmount && (
+                        <div className="flex h-5 items-center gap-1 truncate">
+                          <span className="truncate line-through">
+                            Rs.{_product.priceInAmount}
+                          </span>
 
-                      <span className="truncate text-xs">
-                        {_product.discountPercentage &&
-                          `-${_product.discountPercentage}%`}
-                      </span>
-                    </div>
-                  )}
-                  <div className="flex h-6 items-center justify-start">
-                    <div className="flex h-4">
-                      <Rating
-                        style={{ maxWidth: 65 }}
-                        value={_product.averageReview}
-                        readOnly
-                      />
-                    </div>
+                          <span className="truncate text-xs">
+                            {_product.discountPercentage &&
+                              `-${_product.discountPercentage}%`}
+                          </span>
+                        </div>
+                      )}
+                      <div className="flex h-6 items-center justify-start">
+                        <div className="flex h-4">
+                          <Rating
+                            style={{ maxWidth: 65 }}
+                            value={_product.averageReview}
+                            readOnly
+                          />
+                        </div>
 
-                    <div className="flex h-4 items-center text-muted-foreground">
-                      <span className="text-xs">{_product.averageReview}</span>
-                      <span className="text-[10px] text-muted-foreground">
-                        {`(${_product.totalReviews})`}
-                      </span>
-                    </div>
-                  </div>
-                </CardContent>
-              </Card>
-            </Link>
-          </Button>
-        );
-      })}
+                        <div className="flex h-4 items-center text-muted-foreground">
+                          <span className="text-xs">
+                            {_product.averageReview}
+                          </span>
+                          <span className="text-[10px] text-muted-foreground">
+                            {`(${_product.totalReviews})`}
+                          </span>
+                        </div>
+                      </div>
+                    </CardContent>
+                  </Card>
+                </Link>
+              </Button>
+            );
+          },
+        )}
+      </div>
     </div>
   );
 }
