7cdbb8bcb1d62caa38f1f5dc5445f142df2bd7e4
Ahamed <21102277+athuv@users.noreply.github.com>
2024-05-05 19:22:42 +0530

    feat:Add 'auth' route & LoginRegister molecule component

---
+++ app/_components/molecules/LoginRegister.tsx
@@ -0,0 +1,77 @@
+import { Button } from "@/app/_components/atoms/shadcn/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/app/_components/atoms/shadcn/card";
+import { Input } from "@/app/_components/atoms/shadcn/input";
+import { Label } from "@/app/_components/atoms/shadcn/label";
+import {
+  Tabs,
+  TabsContent,
+  TabsList,
+  TabsTrigger,
+} from "@/app/_components/atoms/shadcn/tabs";
+
+function LoginRegister() {
+  return (
+    <div className="px-2 py-2">
+      <Tabs defaultValue="account" className="w-full">
+        <TabsList className="grid w-full grid-cols-2">
+          <TabsTrigger value="account">Account</TabsTrigger>
+          <TabsTrigger value="password">Password</TabsTrigger>
+        </TabsList>
+        <TabsContent value="account">
+          <Card>
+            <CardHeader>
+              <CardTitle>Account</CardTitle>
+              <CardDescription>
+                Make changes to your account here. Click save when youre done.
+              </CardDescription>
+            </CardHeader>
+            <CardContent className="space-y-2">
+              <div className="space-y-1">
+                <Label htmlFor="name">Name</Label>
+                <Input id="name" defaultValue="Pedro Duarte" />
+              </div>
+              <div className="space-y-1">
+                <Label htmlFor="username">Username</Label>
+                <Input id="username" defaultValue="@peduarte" />
+              </div>
+            </CardContent>
+            <CardFooter>
+              <Button>Save changes</Button>
+            </CardFooter>
+          </Card>
+        </TabsContent>
+        <TabsContent value="password">
+          <Card>
+            <CardHeader>
+              <CardTitle>Password</CardTitle>
+              <CardDescription>
+                Change your password here. After saving, youll be logged out.
+              </CardDescription>
+            </CardHeader>
+            <CardContent className="space-y-2">
+              <div className="space-y-1">
+                <Label htmlFor="current">Current password</Label>
+                <Input id="current" type="password" />
+              </div>
+              <div className="space-y-1">
+                <Label htmlFor="new">New password</Label>
+                <Input id="new" type="password" />
+              </div>
+            </CardContent>
+            <CardFooter>
+              <Button>Save password</Button>
+            </CardFooter>
+          </Card>
+        </TabsContent>
+      </Tabs>
+    </div>
+  );
+}
+export default LoginRegister;
---
+++ app/auth/page.tsx
@@ -0,0 +1,13 @@
+import LoginRegister from "@/app/_components/molecules/LoginRegister";
+
+function AuthPage() {
+  return (
+    <main>
+      <section className="w-full">
+        <LoginRegister />
+      </section>
+    </main>
+  );
+}
+
+export default AuthPage;
