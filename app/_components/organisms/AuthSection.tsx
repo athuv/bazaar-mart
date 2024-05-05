9818aff64b05e10fe1da281cf576991773a44aa6
Ahamed <21102277+athuv@users.noreply.github.com>
2024-05-05 19:59:41 +0530

    feat:Add AuthSection organism component & implement LoginRegister comp..

---
+++ app/_components/organisms/AuthSection.tsx
@@ -0,0 +1,12 @@
+import LoginRegister from "@/app/_components/molecules/LoginRegister";
+
+function AuthSection() {
+  return (
+    <section className="w-full">
+      <div className="flex justify-center">
+        <LoginRegister />
+      </div>
+    </section>
+  );
+}
+export default AuthSection;
