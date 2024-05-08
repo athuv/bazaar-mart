cb0103dff4b987449546e67a41a50217e28467bc
Ahamed <21102277+athuv@users.noreply.github.com>
2024-05-08 21:58:49 +0530

    feat:Add LoginForm molecules component

---
+++ app/_components/molecules/LoginForm.tsx
@@ -0,0 +1,61 @@
+"use client";
+
+import FormInputWithLabel from "@/app/_components/atoms/input/FormInputWithLabel";
+import { Button } from "@/app/_components/atoms/shadcn/button";
+import { Form } from "@/app/_components/atoms/shadcn/form";
+
+import { LoginSchema, loginSchema } from "@/lib/zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+
+function LoginForm() {
+  const form = useForm<LoginSchema>({
+    mode: "onTouched",
+    resolver: zodResolver(loginSchema),
+  });
+
+  const onFormSubmit = async (data: LoginSchema) => {
+    console.log(data);
+  };
+
+  return (
+    <div>
+      <Form {...form}>
+        <form
+          className="flex flex-col"
+          onSubmit={form.handleSubmit(onFormSubmit)}
+        >
+          <FormInputWithLabel
+            name="usernameEmail"
+            form={form}
+            label="Username or email"
+            placeholder="Username or email"
+            type="text"
+            isRequired={true}
+            validation={{
+              error: form.formState.errors.usernameEmail,
+              touchedField: form.formState.touchedFields.usernameEmail,
+            }}
+          />
+          <FormInputWithLabel
+            name="password"
+            form={form}
+            label="Password"
+            placeholder="Password"
+            type="password"
+            isRequired={true}
+            validation={{
+              error: form.formState.errors.password,
+              touchedField: form.formState.touchedFields.password,
+            }}
+          />
+
+          <Button className="mt-2" type="submit">
+            Login
+          </Button>
+        </form>
+      </Form>
+    </div>
+  );
+}
+export default LoginForm;
