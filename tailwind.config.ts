c2d3e9793aadfaa2a3267db114e61103cd71b8e0
Ahamed <21102277+athuv@users.noreply.github.com>
2024-05-08 21:53:23 +0530

    feat:Add custom color varial '--success' (hsl(120, 100%, 30%))

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -52,6 +52,9 @@ const config = {
           DEFAULT: "hsl(var(--card))",
           foreground: "hsl(var(--card-foreground))",
         },
+        success: {
+          DEFAULT: "hsl(var(--success))",
+        },
       },
       borderRadius: {
         lg: "var(--radius)",
