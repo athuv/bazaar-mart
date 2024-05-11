dc7151f4b73ac9c857d8f4a4871935f80ebdf446
Ahamed <21102277+athuv@users.noreply.github.com>
2024-05-11 22:29:55 +0530

    style: Add 'min-width' for logo container

--- app/_components/organisms/desktop/HeaderDesktop.tsx
+++ app/_components/organisms/desktop/HeaderDesktop.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import DeliveryLocationAndSearchBar from "@/app/_components/molecules/desktop/DeliveryLocationAndSearchBar";
+import DesktopSearchBar from "@/app/_components/molecules/desktop/DesktopSearchBar";
 import MobileLogo from "@/app/_components/atoms/logo/MobileLogo";
 import HeaderRightContent from "@/app/_components/molecules/desktop/HeaderRightContent";
 
@@ -8,10 +8,10 @@ function HeaderDektop() {
   return (
     <div className="hidden w-screen bg-primary text-primary-foreground lg:block">
       <div className="flex h-14 items-center justify-between lg:mx-auto lg:max-w-6xl lg:px-2 xl:max-w-7xl">
-        <div className="flex">
+        <div className="flex min-w-52">
           <MobileLogo />
         </div>
-        <DeliveryLocationAndSearchBar />
+        <DesktopSearchBar />
         <HeaderRightContent />
       </div>
     </div>
