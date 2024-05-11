2dd8baaf1e014ff02270fb333b078e5d1c99bbbe
Ahamed <21102277+athuv@users.noreply.github.com>
2024-05-11 22:55:38 +0530

    feat:Move ProductList header to ProductListSection component

--- app/_components/organisms/mobile/ProductListSection.tsx
+++ app/_components/organisms/mobile/ProductListSection.tsx
@@ -4,6 +4,7 @@ import React from "react";
 function ProductListSection() {
   return (
     <section className="w-full">
+      <h2 className="pl-4 pt-2 text-2xl font-semibold">Available Products</h2>
       <ProductList />
     </section>
   );
