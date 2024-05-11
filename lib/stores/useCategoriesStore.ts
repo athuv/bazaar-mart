2126b17dd44abd14394d7091429c486fdcc42874
Ahamed <21102277+athuv@users.noreply.github.com>
2024-05-11 21:50:13 +0530

    feat:Add zustand store called useCategoriesStore

---
+++ lib/stores/useCategoriesStore.ts
@@ -0,0 +1,40 @@
+import { create } from "zustand";
+import { persist } from "zustand/middleware";
+
+import {
+  getAllCategories,
+  getMainCategories,
+} from "@/lib/db/drizzle/queryActions";
+
+import { Category } from "@/lib/types/types";
+
+type State = {
+  mainCategories: Category[];
+  categoriesTree: Category[];
+};
+
+type Actions = {
+  fetchMainCategories: (limit?: number) => Promise<void>;
+  fetchCategoryTree: () => Promise<void>;
+};
+
+export const useCategoriesStore = create<State & Actions>()(
+  persist(
+    (set) => ({
+      mainCategories: [],
+      categoriesTree: [],
+      fetchMainCategories: async (limit = 0) => {
+        const mainCategories: Category[] = await getMainCategories({ limit });
+        set((state) => ({ mainCategories }));
+      },
+      fetchCategoryTree: async () => {
+        const categoriesTree: Category[] = await getAllCategories();
+        set((state) => ({ categoriesTree }));
+      },
+    }),
+    {
+      name: "categories-store",
+      skipHydration: true,
+    },
+  ),
+);
