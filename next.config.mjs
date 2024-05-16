18da9b3506c025b21bd71bd8742e10a79aa18ff3
Ahamed <21102277+athuv@users.noreply.github.com>
2024-05-16 19:26:56 +0530

    feat:Configure NextJS to use supabase storage

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "hjziiwmkcssstjzmgnad.supabase.co",
+        port: "",
+        pathname: "/storage/v1/object/public/bz-mart/**",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
