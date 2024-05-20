d66b847005d5ff641e8486eb1f884d1b10dadcec
Ahamed <21102277+athuv@users.noreply.github.com>
2024-05-20 20:27:31 +0530

    feat:Add AlertbarSection organism component

---
+++ app/_components/organisms/AlertbarSection.tsx
@@ -0,0 +1,12 @@
+import Alertbar from "@/app/_components/molecules/Alertbar";
+
+function AlertbarSection() {
+  return (
+    <section className="w-full">
+      <div className="flex w-full flex-col items-center justify-center">
+        <Alertbar />
+      </div>
+    </section>
+  );
+}
+export default AlertbarSection;
