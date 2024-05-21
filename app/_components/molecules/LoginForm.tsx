506f9be2db9f1949c2350de5c53d9aa52bf8b900
Ahamed <21102277+athuv@users.noreply.github.com>
2024-05-21 11:42:20 +0530

    refacotor:Implement modularized supabase authentication & validations

--- app/_components/molecules/LoginForm.tsx
+++ app/_components/molecules/LoginForm.tsx
@@ -2,14 +2,23 @@
 
 import FormInputWithLabel from "@/app/_components/atoms/input/FormInputWithLabel";
 import { Button } from "@/app/_components/atoms/shadcn/button";
-import { Form } from "@/app/_components/atoms/shadcn/form";
+import { Form, FormMessage } from "@/app/_components/atoms/shadcn/form";
 
 import { LoginSchema, loginSchema } from "@/lib/zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 
 import { useForm } from "react-hook-form";
+import { cn } from "@/lib/utils";
+import { login } from "@/lib/actions/authAction";
+import { useState } from "react";
+import { RotateCw } from "lucide-react";
 
 function LoginForm() {
+  const [serverMessage, setServerMessage] = useState<{
+    type: "error" | "success" | null;
+    message: string | null;
+  }>({ type: null, message: null });
+
   const form = useForm<LoginSchema>({
     mode: "onTouched",
     defaultValues: {
@@ -20,46 +29,77 @@ function LoginForm() {
   });
 
   const handleFormSubmit = async (data: LoginSchema) => {
-    //const result = await login(data);
+    setServerMessage({ type: null, message: null });
+    const result = await login(data);
+    console.log(result);
+    if (Array.isArray(result)) {
+      result?.forEach((err) => {
+        if ((err.type = "field-error")) {
+          form.setError(err.field!, {
+            message: err.message,
+          });
+        }
+      });
+    } else {
+      result?.type === "server-error" &&
+        setServerMessage({ type: "error", message: result.message! });
+    }
   };
 
   return (
-    <div>
-      <Form {...form}>
-        <form
-          onSubmit={form.handleSubmit(handleFormSubmit)}
-          className="flex flex-col"
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(handleFormSubmit)}
+        className="flex flex-col"
+      >
+        <FormInputWithLabel
+          name="email"
+          form={form}
+          label="Email"
+          placeholder="Email"
+          type="email"
+          isRequired={false}
+          validation={{
+            error: form.formState.errors.email,
+            touchedField: form.formState.touchedFields.email,
+          }}
+        />
+        <FormInputWithLabel
+          name="password"
+          form={form}
+          label="Password"
+          placeholder="Password"
+          type="password"
+          isRequired={false}
+          validation={{
+            error: form.formState.errors.password,
+            touchedField: form.formState.touchedFields.password,
+          }}
+        />
+        <FormMessage
+          className={cn(
+            "pl-1 pt-1 text-xs",
+            serverMessage.type === "error" && "text-destructive",
+          )}
         >
-          <FormInputWithLabel
-            name="email"
-            form={form}
-            label="Email"
-            placeholder="Email"
-            type="email"
-            isRequired={false}
-            validation={{
-              error: form.formState.errors.email,
-              touchedField: form.formState.touchedFields.email,
-            }}
-          />
-          <FormInputWithLabel
-            name="password"
-            form={form}
-            label="Password"
-            placeholder="Password"
-            type="password"
-            isRequired={false}
-            validation={{
-              error: form.formState.errors.password,
-              touchedField: form.formState.touchedFields.password,
-            }}
-          />
-          <Button className="mt-2" type="submit">
-            Login
-          </Button>
-        </form>
-      </Form>
-    </div>
+          {serverMessage.message}
+        </FormMessage>
+        <Button
+          disabled={form.formState.isSubmitting}
+          className="mt-2"
+          type="submit"
+        >
+          {form.formState.isSubmitting ? (
+            <div className="flex gap-1">
+              <span>Logging In...</span>
+              <RotateCw className=" animate-spin" />
+            </div>
+          ) : (
+            "Login"
+          )}
+        </Button>
+      </form>
+    </Form>
   );
 }
 export default LoginForm;
