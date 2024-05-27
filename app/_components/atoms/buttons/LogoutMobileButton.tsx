9fdb125b1654e2819f8fd900a31f834f23827c1f
Ahamed <21102277+athuv@users.noreply.github.com>
2024-05-27 20:37:37 +0530

    refactor:Rename 'LogoutButton' component to 'LogoutMobileButton'

--- app/_components/atoms/buttons/LogoutMobileButton.tsx
+++ app/_components/atoms/buttons/LogoutMobileButton.tsx
@@ -4,7 +4,7 @@ import { Button } from "@/app/_components/atoms/shadcn/button";
 import { SheetClose } from "@/app/_components/atoms/shadcn/sheet";
 import { logout } from "@/lib/actions/authAction";
 
-function LogoutButton() {
+function LogoutMobileButton() {
   return (
     <form className="w-full" action={() => logout()}>
       <Button type="submit" className="w-full" variant="secondary" asChild>
@@ -13,4 +13,4 @@ function LogoutButton() {
     </form>
   );
 }
-export default LogoutButton;
+export default LogoutMobileButton;
