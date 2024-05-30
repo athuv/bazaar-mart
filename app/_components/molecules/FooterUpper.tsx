1fca9168cede1de245e97c8f59f48c4d9768c176
Ahamed <21102277+athuv@users.noreply.github.com>
2024-05-30 17:08:16 +0530

    style:Change FooterUpper component view to row & add scroll

--- app/_components/molecules/FooterUpper.tsx
+++ app/_components/molecules/FooterUpper.tsx
@@ -1,23 +1,29 @@
 import { Button } from "@/app/_components/atoms/shadcn/button";
+import {
+  ScrollArea,
+  ScrollBar,
+} from "@/app/_components/atoms/shadcn/scroll-area";
 import Link from "next/link";
-import React from "react";
 
 function FooterUpper() {
   return (
-    <div className="flex flex-col items-start md:flex-row md:items-center md:justify-center md:gap-2">
-      <Button variant="link" className="px-0 text-primary-foreground" asChild>
-        <Link href="./">Home</Link>
-      </Button>
-      <Button variant="link" className="px-0 text-primary-foreground" asChild>
-        <Link href="./login-signin">Sign in / Register</Link>
-      </Button>
-      <Button variant="link" className="px-0 text-primary-foreground" asChild>
-        <Link href="./sell-item">Sell an item</Link>
-      </Button>
-      <Button variant="link" className="px-0 text-primary-foreground" asChild>
-        <Link href="./help-contact">Help & Contact</Link>
-      </Button>
-    </div>
+    <ScrollArea className="whitespace-nowrap">
+      <div className="flex flex-row items-center justify-center gap-2">
+        <Button variant="link" className="px-0 text-primary-foreground" asChild>
+          <Link href="./">Home</Link>
+        </Button>
+        <Button variant="link" className="px-0 text-primary-foreground" asChild>
+          <Link href="./login-signin">Sign in / Register</Link>
+        </Button>
+        <Button variant="link" className="px-0 text-primary-foreground" asChild>
+          <Link href="./sell-item">Sell an item</Link>
+        </Button>
+        <Button variant="link" className="px-0 text-primary-foreground" asChild>
+          <Link href="./help-contact">Help & Contact</Link>
+        </Button>
+      </div>
+      <ScrollBar className="h-1" orientation="horizontal" />
+    </ScrollArea>
   );
 }
 
