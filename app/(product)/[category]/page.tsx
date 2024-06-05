b7ac6f8baf0a7c658222c3102356bdfa3d96bdbd
Ahamed <21102277+athuv@users.noreply.github.com>
2024-06-05 22:22:57 +0530

    feat:Implement MobileFilter component in category root page

--- app/(product)/[category]/page.tsx
+++ app/(product)/[category]/page.tsx
@@ -1,4 +1,11 @@
+import MobileFilter from "@/app/_components/molecules/mobile/MobileFilter";
+
 function CategoryPage({ params }: { params: { category: string } }) {
-  return <div>{params.category}</div>;
+  return (
+    <div className="flex-grow">
+      <div>UNDER DEVELOPMENT</div>
+      <MobileFilter />
+    </div>
+  );
 }
 export default CategoryPage;
