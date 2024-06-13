a5c1da33fe39c749bc9bdbbe528a92597de569df
Ahamed <21102277+athuv@users.noreply.github.com>
2024-06-13 17:51:21 +0530

    refactor: remove deleted AuthSubscriber component from root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,6 @@ import HeaderDektop from "@/app/_components/organisms/desktop/HeaderDesktop";
 import HeaderMobile from "@/app/_components/organisms/mobile/HeaderMobile";
 import AlertbarSection from "@/app/_components/organisms/AlertbarSection";
 import { Analytics } from "@vercel/analytics/react";
-import AuthSubscriber from "@/app/auth/AuthSubscriber";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -25,7 +24,6 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${inter.className} flex min-h-screen flex-col`}>
         <header className="sticky top-0 z-10">
-          {/* <AuthSubscriber /> */}
           <HeaderDektop />
           <HeaderMobile />
         </header>
